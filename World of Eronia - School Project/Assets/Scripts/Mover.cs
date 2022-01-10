4b2c9e2c168f25378f7e5ebde002444c2ad8a5f7
Milán Kormány <kormany.1993@gmail.com>
2022-01-10 21:23:54 +0100

    Setting up Terrain, Adding NevMash Component to player
    - Testing NevMeshAgent with a target prefab
    - Adding new Terrain to the sandbox sceen

---
+++ World of Eronia - School Project/Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
