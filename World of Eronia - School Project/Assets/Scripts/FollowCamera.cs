45b669a5a4eac50d1b7ad8038d727cdb4776bfa5
Milán Kormány <kormany.1993@gmail.com>
2022-01-10 22:35:43 +0100

    Setting Up Camera Follow for Player Prefab
    - Camera now follows the player target.
    - Renamed Sample Scene to Sand Box
    - Rearranged some files

---
+++ World of Eronia - School Project/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position;
+    }
+}
