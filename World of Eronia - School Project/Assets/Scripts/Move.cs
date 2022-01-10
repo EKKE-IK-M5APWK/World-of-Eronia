ea1510fd94756f155cafa89580cdbc366cbb81e8
Milán Kormány <kormany.1993@gmail.com>
2022-01-10 21:55:01 +0100

    Refactoring Movement Script
    - Point Click Ray Cast for movement

---
+++ World of Eronia - School Project/Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}
