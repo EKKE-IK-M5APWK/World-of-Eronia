10c07baa74da291da16cc95dc5b1d5db561694f9
Milán Kormány <kormany.1993@gmail.com>
2022-01-17 18:13:22 +0100

    Fixing Movement
    - It work now as Movement on Hold

--- World of Eronia - School Project/Assets/Scripts/Move.cs
+++ World of Eronia - School Project/Assets/Scripts/Move.cs
@@ -8,7 +8,7 @@ public class Move : MonoBehaviour
     [SerializeField] Transform target;
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
