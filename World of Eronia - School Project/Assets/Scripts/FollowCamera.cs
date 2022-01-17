91c7b7c59dc5c2a2f129075f79f06a0e0aebf6f2
Milán Kormány <kormany.1993@gmail.com>
2022-01-17 18:12:55 +0100

    Fixing Animation studder

--- World of Eronia - School Project/Assets/Scripts/FollowCamera.cs
+++ World of Eronia - School Project/Assets/Scripts/FollowCamera.cs
@@ -10,7 +10,7 @@ public class FollowCamera : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = target.position;
     }
